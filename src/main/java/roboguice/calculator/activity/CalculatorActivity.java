3bb9617266dab97a2c2b87f31f8a7cc68b11437e
Mike Burton <mburton@groupon.com>
2011-11-05 17:48:38 -0700

    Working on saving stack to prefs

--- src/main/java/roboguice/calculator/activity/CalculatorActivity.java
+++ src/main/java/roboguice/calculator/activity/CalculatorActivity.java
@@ -1,9 +1,14 @@
 package roboguice.calculator.activity;
 
 import roboguice.activity.RoboActivity;
+import roboguice.activity.event.OnPauseEvent;
+import roboguice.activity.event.OnResumeEvent;
 import roboguice.calculator.R;
+import roboguice.event.Observes;
 import roboguice.inject.InjectView;
+import roboguice.util.Ln;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +23,7 @@ public class CalculatorActivity extends RoboActivity {
     @InjectView(R.id.tape) TextView tapeView;
     @InjectView(R.id.enter) Button enterButton;
 
-    @Inject Stack<Integer> stack;
+    @Inject RpnStack stack;
 
     String digitAccumulator = "";
 
@@ -75,4 +80,43 @@ public class CalculatorActivity extends RoboActivity {
         enterButton.setEnabled(digitAccumulator.length() > 0);
     }
 
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Ln.d("onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Ln.d("onResume");
+    }
+}
+
+
+class RpnStack extends Stack<Integer> {
+    @Inject SharedPreferences prefs;
+
+    /**
+     * Save to prefs automatically on pause
+     */
+    protected void onPause( @Observes OnPauseEvent ignored) {
+        Ln.d("RpnStack.onPause");
+        final SharedPreferences.Editor edit = prefs.edit();
+        
+        for( int i=0; i<size(); ++i )
+            edit.putInt(String.valueOf(i), get(i));
+
+        edit.commit();
+    }
+
+    /**
+     * Restore from prefs automatically on resume
+     */
+    protected void onResume( @Observes OnResumeEvent ignored ) {
+        Ln.d("RpnStack.onResume");
+        for( int i=0; prefs.contains(String.valueOf(i)); ++i)
+            insertElementAt(prefs.getInt(String.valueOf(i),0), i);
+    }
 }
